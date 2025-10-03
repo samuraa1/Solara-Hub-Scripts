4af33cc7a95e5886fd0e74a89e2b4f2800475e21
samuraa1 <artemivanisovlol@gmail.com>
2025-10-04 00:06:12 +0300

    Update BARP-Copy.lua

--- BARP-Copy.lua
+++ BARP-Copy.lua
@@ -1,1 +1,2 @@
+setclipboard('2025 Project Sunterium 2025')
 setclipboard('loadstring(game:HttpGet("https://raw.githubusercontent.com/ProjectSunterium/Project-Sunterium/refs/heads/main/Project%20Sunterium"))()')
