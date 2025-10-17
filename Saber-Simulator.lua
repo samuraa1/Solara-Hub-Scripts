2f7f08a5d9caca5ec391e81ed03f0b7b14118ac7
samuraa1 <artemivanisovlol@gmail.com>
2025-10-18 00:11:52 +0300

    Create Saber-Simulator.lua

---
+++ Saber-Simulator.lua
@@ -0,0 +1,16 @@
+getgenv().CONFIG = {
+    swing = false,
+    sell = false,
+    collect = false,
+    farmBoss = false,
+    firefarm = false,
+    dungeon = false,
+    buySaber = false,
+    buyBelts = false,
+    buyClass = false,
+    buyAuras = false,
+    buyBossHits = false,
+    buyPetAuras = false,
+    dungeonLevel = 1,
+}
+loadstring(game:HttpGet("https://gist.githubusercontent.com/Curry9206/eb2d9f2730b6437d0a5041b04cb912ee/raw/c0a3bddf5568559f69eecd97d866dc583a3086a3/SaberSimulator"))()
